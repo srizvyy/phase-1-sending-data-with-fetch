d68a30f996f79120c1d4d04ea2bf18d582415a68
Saim Rizvi <shr2mz@gmail.com>
2021-10-02 20:44:24 -0400

    commit tree 935887414bfb4d9a7e452134cd92de65453cd507

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Add your code here
+const postRequestBody = {
+    name: 'Steve',
+    email: 'steve@steve.com'
+}
+
+
+const postRequest = {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        accept: 'application/json'
+    },
+    body: JSON.stringify(postRequestBody),
+
+  
+    
+}
+
+
+function submitData () {
+
+fetch('http://localhost:3000/users', postRequest)
+    .then(res => res.json())
+    .then(data => data(data))
+    .catch((error) => {
+        alert('Unauthorized Access')
+        return error
+    })
+}
+
+function data(newId) {
+    const body = document.getElementsByTagName('body')
+    const p = document.createElement('p')
+
+    p.textContent = newId.id
+    body.appendChild(p)
+}
